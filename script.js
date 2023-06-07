f7d85d99f470d547666565af9c6d9927a43a733c
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-07 14:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let info=document.getElementById("browser-info");
+let browerName=navigator.appName;
+let browerVersion = navigator.appVersion;
+info.innerText=`you are using ${browerName} version ${browerVersion}`;
